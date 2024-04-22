2ea8b86e1a5f243681873c974e22a5a431005315
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 09:08:01 +0900

    clean: reset app

--- app.py
+++ app.py
@@ -2,13 +2,11 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(160, 120)
-        self.x = 0
+        pyxel.init(108, 192)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        pass
 
     def draw(self):
         pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
