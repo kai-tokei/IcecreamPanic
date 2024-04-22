f392f6a74ab524bb478374be33988b7ab4de509e
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 13:14:30 +0900

    add: cone

---
+++ components/cone.py
@@ -0,0 +1,13 @@
+import pyxel
+
+from objects.gameobject import GameObject
+
+class Cone(GameObject):
+    def __init__(self, x: float=75, y: float=122):
+        super().__init__(x, y)
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
