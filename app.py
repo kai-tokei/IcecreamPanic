02a9d4f8b99660647a208e7cf34010d1225df37f
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 09:37:32 +0900

    add: project setting

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import pyxel
 
 class App:
     def __init__(self):
-        pyxel.init(108, 192)
+        pyxel.init(108, 192, fps=60)
+        pyxel.mouse(visible=True)
         pyxel.run(self.update, self.draw)
 
     def update(self):
