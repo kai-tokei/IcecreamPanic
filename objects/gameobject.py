4f09821e92586846b2a49e333d6acc670edfb600
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 09:08:19 +0900

    remove: image

--- objects/gameobject.py
+++ objects/gameobject.py
@@ -2,13 +2,15 @@ import pyxel
 from objects.image import Image
 
 class GameObject:
-    def __init__(self, x: float, y: float, image: Image):
+    def __init__(
+            self,
+            x: float,
+            y: float):
         self.x: float = x
         self.y: float = y
-        self.image: Image = image
 
     def update(self):
         pass
 
     def draw(self):
-        pyxel.blt(self.x, self.y, 0, *self.image)
+        pass
