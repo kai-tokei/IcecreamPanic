16943a0ffc7ecf228f4672632c7c6b135edd915a
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 11:57:57 +0900

    add: button

--- objects/button.py
+++ objects/button.py
@@ -6,3 +6,11 @@ class Button:
         self.y: int = y
         self.width: int = width
         self.height: int = height
+
+    def isInside(self, x: int, y: int):
+        xInside = self.x <= x <= self.x + self.width
+        yInside = self.y <= y <= self.y + self.height
+        return xInside and yInside
+
+    def isClicked(self, x: int, y: int):
+        return pyxel.btnp(pyxel.MOUSE_BUTTON_LEFT) and self.isInside(x, y)
