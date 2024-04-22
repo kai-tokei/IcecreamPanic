4f4c35b7f7ebba1f968ca0d981c4b3cf7faf648d
RitsuMurakami <beinosuke01@gmail.com>
2024-04-22 09:34:58 +0900

    add: CupButton

---
+++ components/button/cup_button.py
@@ -0,0 +1,32 @@
+import pyxel
+from objects.gameobject import GameObject
+
+class CupButton(GameObject):
+    def __init__(self, x: float, y: float):
+        super().__init__(x, y)
+        self.width: int = 16
+        self.height: int = 18
+        self.col: int = 7
+
+    def draw(self):
+        pyxel.line(
+            self.x + self.width * 1/6,
+            self.y,
+            self.x + self.width * 5/6,
+            self.y,
+            self.col
+        )
+        pyxel.line(
+            self.x + self.width * 1/6,
+            self.y,
+            self.x,
+            self.y + self.height,
+            self.col
+        )
+        pyxel.line(
+            self.x + self.width * 5/6,
+            self.y,
+            self.x + self.width,
+            self.y + self.height,
+            self.col
+        )
