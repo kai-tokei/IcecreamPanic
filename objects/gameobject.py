9c7732bd20ba74dc4c775ab4f641473c64aa9aed
RitsuMurakami <beinosuke01@gmail.com>
2024-04-29 17:56:48 +0900

    add: blt in gameobject

--- objects/gameobject.py
+++ objects/gameobject.py
@@ -5,12 +5,29 @@ class GameObject:
     def __init__(
             self,
             x: float,
-            y: float):
+            y: float,
+            img: int=-1,
+            u: int=0,
+            v: int=5,
+            w: int=5,
+            h: int=5,
+            colkey: int=None,
+            ):
         self.x: float = x
         self.y: float = y
+        self.img = img
+        self.u = u
+        self.v = v
+        self.w = w
+        self.h = h
+        self.colkey = colkey
 
     def update(self):
         pass
 
     def draw(self):
-        pass
+        if self.img > 0:
+            if self.colkey == None:
+                pyxel.blt(self.x, self.y, self.img, self.u, self.v, self.w, self.h)
+            else:
+                pyxel.blt(self.x, self.y, self.img, self.u, self.v, self.w, self.h, self.colkey)
