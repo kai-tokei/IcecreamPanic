f00be5c15eb88217e6d02867cf0b955cd8fcba5e
RitsuMurakami <beinosuke01@gmail.com>
2024-05-06 11:06:08 +0900

    add: manager

--- app.py
+++ app.py
@@ -2,18 +2,18 @@
 import pyxel
 
 # object
-from objects.manager import Manager
+from objects.game import Game
 
 class App:
     def __init__(self):
-        self.manager = Manager()
+        self.Game = Game()
         pyxel.init(108, 192, fps=60)
         pyxel.load("assets/icecream.pyxres")
         pyxel.mouse(visible=True)
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.manager.update()
+        self.Game.update()
 
     def draw(self):
-        self.manager.draw()
+        self.Game.draw()
