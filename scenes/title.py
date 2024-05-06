3c833271047c0fa7582dda8288d45476daeb98ff
RitsuMurakami <beinosuke01@gmail.com>
2024-05-06 12:52:14 +0900

    add: flick

--- scenes/title.py
+++ scenes/title.py
@@ -1,12 +1,25 @@
 import pyxel
+import random
 
 class Title:
     def __init__(self) -> None:
-        pass
+        self.text: int = 0
+        self.image: int = 0
 
     def update(self):
         pass
 
     def draw(self):
+        self.text += random.randint(1, 3)
+        self.image += random.randint(1, 3)
+
         pyxel.cls(1)
-        pyxel.blt(0, -15, 1, 0, 0, 108, 192)
+        if self.text < 149:
+            pyxel.blt(0, 20, 1, 0, 24, 107, 50)
+        elif self.text > 150:
+            self.text = 0
+
+        if self.image < 149:
+            pyxel.blt(16, 74, 1, 16, 72, 95, 143)
+        elif self.image > 150:
+            self.image = 0
